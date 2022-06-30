7d0d1f7d4f465134369ce2312c0c839d17720b78
Percy Ma <kecrily@gmail.com>
2022-06-30 15:20:25 +0800

    feat: add 404 page

---
+++ src/pages/[...all].vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="text-4xl font-bold mb-6">
+    Are you lost?
+  </h1>
+
+  <a-space class="wrapper" direction="vertical">
+    <a-button type="primary" @click="$router.back()">
+      The way back home
+    </a-button>
+
+    <a-button href="https://github.com/kecrily/syesya/issues">
+      A issue? Report!
+    </a-button>
+  </a-space>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
