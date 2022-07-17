d512a019019097a808c3656920680dbb4584a853
Percy Ma <kecrily@gmail.com>
2022-07-17 19:36:55 +0800

    fix: deal with edges about character and note meta

--- src/composables/crossbell.ts
+++ src/composables/crossbell.ts
@@ -4,6 +4,17 @@ const provider = window.ethereum
 const contract = new Contract(provider)
 const indexer = new Indexer()
 
+export interface csbUri {
+  scheme: 'csb://'
+  scope: 'account' | 'asset'
+  identity: string
+  host: string
+  platform: string | undefined
+  network: string | undefined
+  contract_address: string | undefined
+  token_id: string | undefined
+}
+
 export function resolveIpfsLink(link: string | undefined) {
   if (!link) return undefined
   return `https://gateway.ipfs.io/${link.replace('://', '/')}`
--- src/pages/character/[id].vue
+++ src/pages/character/[id].vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import { BigNumber } from 'ethers'
-import type { CharacterEntity, NoteEntity } from '~/composables/crossbell'
+import type { CharacterEntity, NoteEntity, csbUri } from '~/composables/crossbell'
 import { resolveIpfsLink, utils } from '~/composables/crossbell'
 
 const props = defineProps<{ id: number|string }>()
 let character: CharacterEntity|null = $ref()
 let notes: NoteEntity[] = $ref()
+let social: csbUri[] = $ref()
 
 character = await indexer.getCharacter(BigNumber.from(props.id))
 
-const social = character?.metadata?.content.connected_accounts
-  ?.map(a => utils.parseCsbUri(a))
+if (typeof character?.metadata?.content?.connected_accounts?.[0] === 'string') {
+  social = character?.metadata?.content?.connected_accounts
+    ?.map(a => utils.parseCsbUri(a))
+}
 
 notes = (await indexer.getNotes({ characterId: props.id })).list
 </script>
@@ -18,15 +21,15 @@ notes = (await indexer.getNotes({ characterId: props.id })).list
 <template>
   <div class="flex flex-row items-center space-x-2">
     <a-avatar>
-      <img :src="resolveIpfsLink(character?.metadata?.content.avatars![0])">
+      <img :src="resolveIpfsLink(character?.metadata?.content?.avatars?.[0])">
     </a-avatar>
 
     <h1 class="text-2xl">
-      {{ character?.metadata?.content.name }}
+      {{ character?.metadata?.content?.name }}
     </h1>
   </div>
   <div class="opacity-80 prose">
-    {{ character?.metadata?.content.bio }}
+    {{ character?.metadata?.content?.bio }}
   </div>
 
   <div class="flex flex-row space-x-2 mt-2">
@@ -35,26 +38,26 @@ notes = (await indexer.getNotes({ characterId: props.id })).list
       @{{ character?.handle }}
     </a-tag>
     <a
-      v-for="w in character?.metadata?.content.websites"
+      v-for="w in character?.metadata?.content?.websites"
       :key="w" :href="w" target="_blank" class="no-underline"
     >
       <a-tag>
         <div class="ri:link mr-1" />{{ w.replace(/https?:\/\//, '') }}
       </a-tag>
     </a>
-    <a-tag v-for="s in social" :key="s.host + s.identity">
-      <div :class="`ri:${s.platform}-line`" class="mr-1" />{{ s.identity }}
+    <a-tag v-for="s in social" :key="s?.host + s?.identity">
+      <div :class="`ri:${s?.platform}-line`" class="mr-1" />{{ s?.identity }}
     </a-tag>
   </div>
 
   <div class="mt-4 space-y-6 opacity-90 text-base">
     <article v-for="n in notes" :key="n.linkKey">
-      {{ n.metadata?.content.content }}
+      {{ n.metadata?.content?.content }}
       <a :href="n.toUri?.toString()" class="text-sm no-underline">
         ↩
       </a>
 
-      <a-carousel v-if="n.metadata?.content.attachments?.length" class="mt-2 h-60 w-150">
+      <a-carousel v-if="n.metadata?.content?.attachments?.length" class="mt-2 h-60 w-150">
         <a-carousel-item
           v-for="a in n.metadata?.content.attachments" :key="a.name"
           class="rounded"
